0df7ff0683a9804ffb75d007a4a15bbf6cec4f8f
enriohernanda <enriohernanda@gmail.com>
2025-09-18 17:07:11 +0700

    update bg footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 export default function Footer() {
   return (
-    <footer className="w-full py-6 bg-white dark:bg-black border-t border-gray-200 dark:border-gray-700 transition-colors duration-300">
+    <footer className="w-full py-6 bg-[#F9FAFB] dark:bg-[#111827] border-t border-gray-200 dark:border-gray-700 transition-colors duration-300">
       <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.6 }} viewport={{ once: true }} className="text-center text-sm text-[#4B5563] dark:text-[#D1D5DB]">
         © 2025 | Created by <span className="font-medium">Enrio Hernanda</span>
       </motion.p>
